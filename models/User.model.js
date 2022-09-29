1c49bb2aa8b29680468186cbab57d30e89f5ab4d
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-29 15:06:54 +0200

    add bcryptjs package and fixed syntax in app.js and auth.routes

--- models/User.model.js
+++ models/User.model.js
@@ -5,7 +5,7 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  name: { type: String, required: true },
+  userName: { type: String, required: true },
   birthday:Date,
   religion:String,
 },
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const express = require("express")
 // ℹ️ Handles password encryption
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 
 // How many rounds should bcrypt run the salt (default [10 - 12 rounds])
@@ -13,12 +13,12 @@ const User = require("../models/User.model");
 
 // POST /auth/signup - Creates a new user in the database
 router.post("/signup", (req, res) => {
-  const { email, password, firstName, lastName, birthday, religion } = req.body;
+  const { email, password, userName } = req.body;
 
-  if (email === '' || password === '' || firstName === '' || lastName === '' || birthday === '' || religion === '') {
+  if (email === '' || password === '' || userName) {
     return res
       .status(400)
-      .json({ errorMessage: "Please provide your email, password, firstName, lastName, birthday and religion." });
+      .json({ errorMessage: "Please provide your email, password, userName" });
   }
 
   if (password.length < 6) {
@@ -32,7 +32,7 @@ router.post("/signup", (req, res) => {
   if (!regex.test(password)) {
     return res
       .status(400)
-      .json({ errorMessage: "Password needs to have at least 8 chars and must contain at least one number, one lowercase and one uppercase letter.",});
+      .json({ errorMessage: "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",});
   }
 
   // Search the database for a user with the email submitted in the form
@@ -50,15 +50,15 @@ router.post("/signup", (req, res) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then` 
-      return User.create({ email, password: hashedPassword, firstName, lastName });
+      return User.create({ email, password: hashedPassword, userName });
     })
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, firstName, lastName, _id } = createdUser;
+      const { email, userName, _id } = createdUser;
     
       // Create a new object that doesn't expose the password
-      const user = { email, firstName, lastName, _id };
+      const user = { email, userName, _id };
 
       // Send a json response containing the user object
       res.status(201).json({ user: user });
@@ -94,10 +94,10 @@ router.post('/login', (req, res, next) => {
 
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
-        const { _id, email, firstName } = foundUser;
+        const { _id, email, userName } = foundUser;
         
         // Create an object that will be set as the token payload
-        const payload = { _id, email, firstName };
+        const payload = { _id, email, userName };
 
         // Create and sign the token
         const authToken = jwt.sign( 
@@ -117,5 +117,16 @@ router.post('/login', (req, res, next) => {
     .catch(err => res.status(500).json({ message: "Internal Server Error" }));
 });
 
+// GET  /auth/verify  -  Used to verify JWT stored on the client
+router.get('/verify', isAuthenticated, (req, res, next) => {       // <== CREATE NEW ROUTE
+ 
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  console.log(`req.payload`, req.payload);
+ 
+  // Send back the object with user data
+  // previously set as the token payload
+  res.status(200).json(req.payload);
+});
 
 module.exports = router;
