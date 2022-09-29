ed4349cbddf76abb87e1ab8bf3613bb997b84e8c
paulifauli <prr@MacBook-Pro-von-Paul.local>
2022-09-29 14:58:38 +0200

    just a catch up no real changes

--- models/User.model.js
+++ models/User.model.js
@@ -1,11 +1,12 @@
 
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
-
 const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  name: { type: String, required: true },
+  userName: { type: String, required: true },
+  firstName:String,
+  lastName:String,
   birthday:Date,
   religion:String,
 },
@@ -14,5 +15,4 @@ const userSchema = new Schema({
   timestamps: true,
 }
 );
-
 module.exports = model("User", userSchema);
