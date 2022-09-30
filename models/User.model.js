e7a66ae797ec2707ba30420664eece37e9e19ef7
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-30 10:39:53 +0200

    recipient endpoint response successful

--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,4 @@
-const mongoose = require(mongoose);
+const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
@@ -19,5 +19,3 @@ const userSchema = new Schema({
 const User = mongoose.model("User", userSchema);
 
 module.exports = User
-
-
--- routes/recipient.routes.js
+++ routes/recipient.routes.js
@@ -3,22 +3,20 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Recipient = require("../models/Recipient")
 
-//  POST /api/lists  -  Creates a new list
-router.post("/lists", (req, res, next) => {
+//  POST /api/recipients  -  Creates a new recipient
+router.post("/recipients", (req, res, next) => {
     const { name, personalDetails, userId, picturePerson, occasion, preference, unwanted, priceSpan, imageGift, title, link, notes } = req.body;
   
     Recipient.create({name, personalDetails, user: userId, picturePerson, occasion, preference, unwanted, priceSpan, imageGift, title, link, notes})
       .then(newRecipient => {
-        return Recipient.findByIdAndUpdate(userId, { $push: { lists: newRecipient._id } } );
+         Recipient.findByIdAndUpdate(userId, { $push: { recipients: newRecipient._id } } );
+         res.json(newRecipient)
       })
-      .then((response) => res.json(response))
+      // .then(() => res.json(newRecipient))
       .catch((err) => res.json(err));
 });
-
-
 
 module.exports = router;
 
 // changed the wording with Marco --> Recipient makes more sense, because with this route we create a new Recipient, not a list 
 // + the route is wrong - see figma to see the name // it should be /addRecipient
-// TODO: receiving null as a response after attempting to create list assigned to user
