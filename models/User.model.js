39df284e859c0ceee0775c0d63281826fd2f45cf
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-30 16:45:57 +0200

    restructure models

--- models/User.model.js
+++ models/User.model.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  id:String,
+const userSchema = new Schema(
+  {
+  id: String,
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   userName: { type: String, required: true },
-  firstName:String,
-  lastName:String,
-  birthday:Date,
-  religion:String,
-},
-{
+  firstName: String,
+  lastName: String,
+  birthday: Date,
+  religion: String,
+  },
+  {
   timestamps: true,
-}
+  }
 );
 
 const User = mongoose.model("User", userSchema);
--- routes/recipient.routes.js
+++ routes/recipient.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const { request } = require("../app");
 const Recipient = require("../models/Recipient.model")
 const User = require("../models/User.model")
 
