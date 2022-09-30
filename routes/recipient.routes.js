6f38b8537dee399c9a795329ea7da122dd437367
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-30 11:53:09 +0200

    add CRUD recipient rouetes

--- routes/recipient.routes.js
+++ routes/recipient.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const Recipient = require("../models/Recipient")
+const { request } = require("../app");
+const Recipient = require("../models/Recipient.model")
+const User = require("../models/User.model")
 
 //  POST /api/recipients  -  Creates a new recipient
 router.post("/recipients", (req, res, next) => {
@@ -12,8 +14,55 @@ router.post("/recipients", (req, res, next) => {
          Recipient.findByIdAndUpdate(userId, { $push: { recipients: newRecipient._id } } );
          res.json(newRecipient)
       })
-      // .then(() => res.json(newRecipient))
       .catch((err) => res.json(err));
+});
+
+// GET /api/recipients - Retrieves all the recipients
+router.get('/recipients', (req, res, next) => {
+  Recipient.find()
+    .then((allRecipients) => res.json(allRecipients))
+    .catch((err) => res.json(err))
+});
+
+// GET /api/recipients/:recipientId - Retrieves a specific recipient by id
+router.get('/recipients/:recipientId', (req, res, next) => {
+  const { recipientId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(recipientId)) {
+    res.status(400).json({ message: 'Specified id is not valid' })
+    return;
+  }
+  Recipient.findById(recipientId)
+    .then(recipient => res.status(200).json(recipient))
+    .catch(error => res.json(error));
+});
+
+// PUT  /api/recipients/:recipientId  -  Updates a specific recipient by id
+router.put('/recipients/:recipientId', (req, res, next) => {
+  const { recipientId } = req.params;
+ 
+  if (!mongoose.Types.ObjectId.isValid(recipientId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+ 
+  Recipient.findByIdAndUpdate(recipientId, req.body, { new: true })
+    .then((updatedRecipient) => res.json(updatedRecipient))
+    .catch(error => res.json(error));
+});
+
+// DELETE  /api/recipients/:recipientId  -  Deletes a specific recipient by id
+router.delete('/recipients/:recipientId', (req, res, next) => {
+  const { recipientId } = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(recipientId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+ 
+  Recipient.findByIdAndRemove(recipientId)
+    .then(() => res.json({ message: `Recipient with ${recipientId} is removed successfully.` }))
+    .catch(error => res.json(error));
 });
 
 module.exports = router;
