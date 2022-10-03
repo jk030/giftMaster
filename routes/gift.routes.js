58780183ed69d73576f05181ad4c64560e6b189f
Kenneth Elumba <kennethelumba@gmail.com>
2022-10-03 10:51:47 +0200

    add post, get, put, delete routes for gifts

--- routes/gift.routes.js
+++ routes/gift.routes.js
@@ -15,3 +15,53 @@ router.post("/gifts", (req, res, next) => {
       })
       .catch((err) => res.json(err));
 });
+
+// GET /api/gifts - Retrieves all the gifts
+router.get('/gifts', (req, res, next) => {
+    Gift.find()
+      .then((allGifts) => res.json(allGifts))
+      .catch((err) => res.json(err))
+});
+
+// GET /api/gifts/:giftId - Retrieves a specific gift by id
+router.get('/gifts/:giftId', (req, res, next) => {
+    const { giftId } = req.params;
+  
+    if (!mongoose.Types.ObjectId.isValid(giftId)) {
+      res.status(400).json({ message: 'Specified id is not valid' })
+      return;
+    }
+    Gift.findById(giftId)
+      .then(gift => res.status(200).json(gift))
+      .catch(error => res.json(error));
+});
+
+// PUT  /api/gifts/:giftId  -  Updates a specific gift by id
+router.put('/gifts/:giftId', (req, res, next) => {
+    const { giftId } = req.params;
+   
+    if (!mongoose.Types.ObjectId.isValid(giftId)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+   
+    Gift.findByIdAndUpdate(giftId, req.body, { new: true })
+      .then((updatedGift) => res.json(updatedGift))
+      .catch(error => res.json(error));
+});
+
+// DELETE  /api/gifts/:giftId  -  Deletes a specific gift by id
+router.delete('/gifts/:giftId', (req, res, next) => {
+    const { giftId } = req.params;
+    
+    if (!mongoose.Types.ObjectId.isValid(giftId)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+   
+    Gift.findByIdAndRemove(giftId)
+      .then(() => res.json({ message: `Gift with ${giftId} is removed successfully.` }))
+      .catch(error => res.json(error));
+});
+
+module.exports = router;
